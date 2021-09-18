f68b00f4a553dff83d522a19675fdfd248becbf8
Jhose Portillo <jhosmar26.00@gmail.com>
2021-09-18 09:08:50 -0500

    EDIT: transform person menu into a component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,27 +4,7 @@
       Ravn Star Wars Registry
     </div>
     <div class="hero">
-      <div class="menu">
-        <div>
-        </div>
-        <div
-          class="menu_item"
-          v-for="person in allPeople.people"
-          :key="person.index"
-          v-on:click="handlePerson(person.id)"
-        >
-          <div>
-            <div class="name">
-              {{ person.name }}
-            </div>
-            <div class="description">
-              {{ person.species ? person.species.name : person.gender }} from {{ person.homeworld.name }}
-            </div>
-          </div>
-          <div class="arrow"></div>
-        </div>
-        <a v-on:click.prevent="loadMore">aaaa</a>
-      </div>
+      <SideMenu/>
       <div class="content">
         General Information
         <!-- <div class="details"></div> -->
@@ -34,12 +14,15 @@
 </template>
 
 <script>
-import gql from "graphql-tag";
+import SideMenu from "./SideMenu.vue"
 
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    SideMenu
   },
   data() {
     return {
@@ -47,65 +30,6 @@ export default {
       showMoreEnabled: true,
     }
   },
-  apollo: {
-    allPeople: gql`
-      query firstFivePeople($after: String){
-        allPeople(first: 5, after: $after){
-          people{
-            id
-            name
-            gender
-            species{
-              name
-              classification
-            }
-            homeworld {
-              name
-            }
-          }
-          pageInfo{
-            endCursor
-          }
-        }
-      }
-    `,
-    // Initial variables
-    variables: {
-      after: null
-    }
-  },
-  methods: {
-    loadMore() {
-      // Fetch more data and transform the original result
-      this.$apollo.queries.allPeople.fetchMore({
-        // New variables
-        variables: {
-          after: this.$apolloData.data.allPeople.pageInfo.endCursor,
-        },
-        // Transform the previous result with new data
-        updateQuery: (previousResult, { fetchMoreResult }) => {
-          const newPageInfo = fetchMoreResult.allPeople.pageInfo.endCursor
-          const newPeople = fetchMoreResult.allPeople.people
-          const hasMore = fetchMoreResult.allPeople.hasMore
-
-          this.showMoreEnabled = hasMore
-
-          return {
-            allPeople: {
-              __typename: previousResult.allPeople.__typename,
-              // Merging the tag list
-              people: [...previousResult.allPeople.people, ...newPeople],
-              hasMore,
-              pageInfo: {
-                __typename: previousResult.allPeople.pageInfo.__typename,
-                endCursor: newPageInfo
-              }
-            },
-          }
-        },
-      })
-    },
-  }
 }
 </script>
 
@@ -122,35 +46,5 @@ export default {
   }
   .hero{
     display: flex;
-  }
-  .menu{
-    border-right: 1px solid #0000001A;
-    min-width: 24rem;
-    height: calc(100vh - 3.45rem);
-    overflow: scroll;
-  }
-  .menu_item{
-    margin-left: 1rem;
-    padding: 1rem 2rem 1rem 0;
-    font-weight: bold;
-    border-bottom: 1px solid #0000001A;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .menu_item .name{
-    font-size: 17px;
-    color: var(--textDark);
-  }
-  .menu_item .description{
-    font-size: 14px;
-    color: var(--textLight);
-  }
-  .arrow{
-    border-top: 2px solid black;
-    border-right: 2px solid black;
-    width: .5rem;
-    height: .5rem;
-    transform: rotate(45deg);
   }
 </style>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="menu">
+    <div
+      class="menu_item"
+      v-for="person in allPeople.people"
+      :key="person.index"
+      v-on:click="handlePerson(person.id)"
+    >
+      <div>
+        <div class="name">
+          {{ person.name }}
+        </div>
+        <div class="description">
+          {{ person.species ? person.species.name : person.gender }} from {{ person.homeworld.name }}
+        </div>
+      </div>
+      <div class="arrow"></div>
+    </div>
+    <a v-on:click.prevent="loadMore">aaaa</a>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: 'SideMenu',
+  data() {
+    return{
+      after: null,
+      showMoreEnabled: true,
+    }
+  },
+  apollo: {
+    allPeople: gql`
+      query firstFivePeople($after: String){
+        allPeople(first: 5, after: $after){
+          people{
+            id
+            name
+            gender
+            species{
+              name
+              classification
+            }
+            homeworld {
+              name
+            }
+          }
+          pageInfo{
+            endCursor
+          }
+        }
+      }
+    `,
+    // Initial variables
+    variables: {
+      after: null
+    }
+  },
+  methods: {
+    loadMore() {
+      // Fetch more data and transform the original result
+      this.$apollo.queries.allPeople.fetchMore({
+        // New variables
+        variables: {
+          after: this.$apolloData.data.allPeople.pageInfo.endCursor,
+        },
+        // Transform the previous result with new data
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const newPageInfo = fetchMoreResult.allPeople.pageInfo.endCursor
+          const newPeople = fetchMoreResult.allPeople.people
+          const hasMore = fetchMoreResult.allPeople.hasMore
+
+          this.showMoreEnabled = hasMore
+
+          return {
+            allPeople: {
+              __typename: previousResult.allPeople.__typename,
+              // Merging the tag list
+              people: [...previousResult.allPeople.people, ...newPeople],
+              hasMore,
+              pageInfo: {
+                __typename: previousResult.allPeople.pageInfo.__typename,
+                endCursor: newPageInfo
+              }
+            },
+          }
+        },
+      })
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.menu{
+    border-right: 1px solid #0000001A;
+    min-width: 24rem;
+    height: calc(100vh - 3.45rem);
+    overflow: scroll;
+  }
+  .menu_item{
+    margin-left: 1rem;
+    padding: 1rem 2rem 1rem 0;
+    font-weight: bold;
+    border-bottom: 1px solid #0000001A;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .menu_item .name{
+    font-size: 17px;
+    color: var(--textDark);
+  }
+  .menu_item .description{
+    font-size: 14px;
+    color: var(--textLight);
+  }
+  .arrow{
+    border-top: 2px solid black;
+    border-right: 2px solid black;
+    width: .5rem;
+    height: .5rem;
+    transform: rotate(45deg);
+  }
+</style>
